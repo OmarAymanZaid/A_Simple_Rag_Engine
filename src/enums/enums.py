ef58a8d67da33d3f9af833aa28519b40f978a0cf
OmarAyman <omaraymanzaid1122@gmail.com>
2026-08-11 13:41:15 +0300

    Add FILE_UPLOAD_SUCCESS and fix typo in FILE_INGEST_FAILED in ResponseSignal enum

--- src/enums/enums.py
+++ src/enums/enums.py
@@ -7,5 +7,7 @@ class ResponseSignal(str, Enum):
     FILE_VALIDATED_SUCCESS = "file_validated_successfully"
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
     FILE_UPLOAD_FAILED = "file_upload_failed"
     FILE_INGEST_SUCCESS = "file_ingest_success"
+    FIle_INGEST_FAILED = "file_ingest_failed"
